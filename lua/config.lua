c95d375caa6d83b29996d6562b6d56abff424727
loej330 <joelittlejr3@gmail.com>
2024-10-23 23:43:06 -0500

    inital commit

---
+++ lua/config.lua
@@ -0,0 +1,29 @@
+-- Line Numbering
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Saving
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- Wrapping
+vim.opt.wrap = false
+
+-- Scrolling
+vim.opt.scrolloff = 10
+
+-- Tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Allow code folding
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevelstart = 999
+
+-- Coloring
+vim.opt.colorcolumn = "80"
+vim.opt.termguicolors = true
+
---
+++ lua/plugins/themes.lua
@@ -0,0 +1,10 @@
+return {
+    { 'echasnovski/mini.base16', version = '*' },
+    { "savq/melange-nvim" },
+    { "doums/darcula" },
+    { "rose-pine/neovim", name = "rose-pine" },    
+    { "rebelot/kanagawa.nvim" },
+    { "xiantang/darcula-dark.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter", },
+    },
+}
